9c9cd62274627935b09338b319eac651a66f5649
Ubuntu <ubuntu@ip-172-31-32-159.eu-north-1.compute.internal>
2024-07-12 08:01:32 +0000

    chat-app updates

--- therapist_chat/therapist_chat/settings.py
+++ therapist_chat/therapist_chat/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-i371l*@*!un=g%o56w$p%%d3^g&b^cx-ahblzbl*%1(ph=$(qe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['13.60.35.232']
 
 # Application definition
 
