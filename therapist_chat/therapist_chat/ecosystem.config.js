39c67272655d236563138df848116f4598ae9b1e
awan-bilal <19muhammdumair@gmail.com>
2024-07-17 01:21:01 +0500

    changes for connection

--- therapist_chat/therapist_chat/ecosystem.config.js
+++ therapist_chat/therapist_chat/ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
     apps: [
       {
         name: "chat-app-daphne",
-        script: "venv/bin/python",
+        script: "../venv/bin/python",
         args: "daphne_config.py",
         interpreter: "none"
       },
       {
         name: "chat-app-gunicorn",
-        script: "venv/bin/gunicorn",
+        script: "../venv/bin/gunicorn",
         args: "therapist_chat.wsgi:application -b 0.0.0.0:8000"
       }
     ]
