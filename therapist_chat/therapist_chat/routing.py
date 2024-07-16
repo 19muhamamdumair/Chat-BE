6c10e572782afbda750492024c904a9a87548194
awan-bilal <19muhammdumair@gmail.com>
2024-07-17 01:04:02 +0500

    changes for connection

--- therapist_chat/therapist_chat/routing.py
+++ therapist_chat/therapist_chat/routing.py
@@ -1,13 +1,16 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 # import app.routing
-import os
+# import os
 from django.core.asgi import get_asgi_application
 from django.urls import re_path
 from chat.consumers import TextRoomConsumer
+from django.core.wsgi import get_wsgi_application
+import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'therapist_chat.settings')
+
+
 websocket_urlpatterns = [
     re_path(r'^ws/chat/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),
-    
 ]
 # the websocket will open at 127.0.0.1:8000/ws/<room_name>
 application = ProtocolTypeRouter({
@@ -18,3 +21,5 @@ application = ProtocolTypeRouter({
         )
     ,
 })
+
+wsgi_application = get_wsgi_application()
--- therapist_chat/therapist_chat/settings.py
+++ therapist_chat/therapist_chat/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-i371l*@*!un=g%o56w$p%%d3^g&b^cx-ahblzbl*%1(ph=$(qe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.60.35.232']
+ALLOWED_HOSTS = ['13.60.35.232','127.0.0.1']
 
 # settings.py
 
@@ -132,7 +132,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'therapist_chat.wsgi.application'
+WSGI_APPLICATION = 'therapist_chat.routing.application'
 
 
 # Database
