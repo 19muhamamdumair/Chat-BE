0e13b5457afcd3781c33430596e472b3e4f8c37e
awan-bilal <19muhammdumair@gmail.com>
2024-07-17 00:38:21 +0500

    changes for routing

--- therapist_chat/therapist_chat/routing.py
+++ therapist_chat/therapist_chat/routing.py
@@ -1,12 +1,17 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
 # import app.routing
+import os
+from django.core.asgi import get_asgi_application
 from django.urls import re_path
 from chat.consumers import TextRoomConsumer
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'therapist_chat.settings')
 websocket_urlpatterns = [
     re_path(r'^ws/chat/(?P<room_name>[^/]+)/$', TextRoomConsumer.as_asgi()),
+    
 ]
 # the websocket will open at 127.0.0.1:8000/ws/<room_name>
 application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
     'websocket':
         URLRouter(
             websocket_urlpatterns
