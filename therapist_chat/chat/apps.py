98806e2a66413700d35cf366f4c5f5282f80b3a6
awan-bilal <19muhammdumair@gmail.com>
2024-07-24 03:19:13 +0500

    changes for msg

--- therapist_chat/chat/apps.py
+++ therapist_chat/chat/apps.py
@@ -1,7 +1,8 @@
 # chat/apps.py
 
 from django.apps import AppConfig
-
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'therapist_chat.settings')
 
 class ChatConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
