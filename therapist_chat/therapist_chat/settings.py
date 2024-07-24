a5926bc6c9326356ab351cf0134992a79b15619e
awan-bilal <19muhammdumair@gmail.com>
2024-07-24 14:11:05 +0500

    changes for import

--- therapist_chat/therapist_chat/settings.py
+++ therapist_chat/therapist_chat/settings.py
@@ -95,7 +95,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'channels',
     'chat',
     'storages',
